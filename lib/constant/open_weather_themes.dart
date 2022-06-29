3b2d3e6ebb4599891cb5d991d3f26f2f73ef8455
John Melody Me <Johnmelodyme@yandex.com>
2022-06-29 12:48:56 +0800

    Create open_weather_themes.dart

---
+++ lib/constant/open_weather_themes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class OpenWeatherThemes {
+  OpenWeatherThemes._();
+
+  static final Color? primaryAccent = Colors.amber[900];
+  static final Color? secondaryAccent = Colors.amber[600];
+  static final Color? errorAccent = Colors.red[900];
+}
