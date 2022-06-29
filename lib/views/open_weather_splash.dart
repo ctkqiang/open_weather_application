0a4d8578fd9e3da2ecf5c30454de7820cc5ac8da
John Melody Me <Johnmelodyme@yandex.com>
2022-06-29 12:48:45 +0800

    Create open_weather_splash.dart

---
+++ lib/views/open_weather_splash.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class OpenWeatherSplashScreen extends StatelessWidget {
+  int? duration;
+  OpenWeatherSplashScreen({Key? key, this.duration}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
